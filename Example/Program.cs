36049fdc2ee4126b84cca4a3a17e7e44088162c5
Keiichiro-Murayama <keiichiro.murayama@wemex.com>
2026-04-27 15:31:23 +0900

    first commit

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,14 +1,28 @@
-﻿namespace Example;
+﻿using System.Runtime.CompilerServices;
+
+namespace Example;
 
 static class Program
 {
     static void Main(string[] args)
-    {
-        int quantity = 10;
-        int price = 5;
-        int total = quantity * price;
-        System.Console.WriteLine(total);
-        
+     {
+    //     string a;
+    //     a = "abcあいうえお";
+    //     string b;
+    //     //変数bに変数aの内容"abcあいうえお"という文字列を格納
+    //     b = $"変数aの内容は{a}";
+    //     string c;
+    //     //空文字列は""で表す
+    //     c = "";
+    //     System.Console.WriteLine(a);
+    //     System.Console.WriteLine(b);
+    //     System.Console.WriteLine(c);
+
+        int x=5;
+        int y=2;
+        float z= (float) x/y;
+        System.Console.Write(z);
+
     }
 }
 
