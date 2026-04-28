accce9e7cbed19864544b4774299be8e8715a9ca
Keiichiro-Murayama <keiichiro.murayama@wemex.com>
2026-04-28 14:53:49 +0900

    Ex_03_01 start

---
+++ Ex_03_01/Item.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics.Contracts;
+
+namespace Ex_03_01;
+
+// <summary>
+// 
+// <summary>
+
+public class  Item
+{
+    public int itemNo;
+    public int itemName;
+    public int price;
+
+}
