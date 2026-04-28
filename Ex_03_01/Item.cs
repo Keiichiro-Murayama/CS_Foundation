69f3a62eee33541a53d68490d599986b5bd47b43
Keiichiro-Murayama <keiichiro.murayama@wemex.com>
2026-04-28 15:10:08 +0900

    finished Ex_03_01

--- Ex_03_01/Item.cs
+++ Ex_03_01/Item.cs
@@ -3,13 +3,13 @@ using System.Diagnostics.Contracts;
 namespace Ex_03_01;
 
 // <summary>
-// 
+//商品情報を表すクラス 
 // <summary>
 
 public class  Item
 {
-    public int itemNo;
-    public int itemName;
-    public int price;
+    public int itemNo; //商品番号
+    public string itemName; //商品名
+    public int price; //単価
 
 }
--- Ex_03_01/Program.cs
+++ Ex_03_01/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace Ex_03_01;
+﻿using System.Net.ServerSentEvents;
+
+namespace Ex_03_01;
 
 static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var item = new Item();
+
+        System.Console.WriteLine("商品番号を入力して下さい->");
+        item.itemNo = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("商品名を入力して下さい->");
+        item.itemName = Console.ReadLine();
+        
+        System.Console.WriteLine("価格を入力して下さい->");
+        item.price = int.Parse(Console.ReadLine());
+
+
+        System.Console.WriteLine("結果");
+        System.Console.WriteLine($"商品番号 = {item.itemNo}");
+        System.Console.WriteLine($"商品名 = {item.itemName}");
+        System.Console.WriteLine($"価格 = {item.price}");
     }
 }
