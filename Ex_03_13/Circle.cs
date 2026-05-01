bea4a4213db61a99e5b1f7dbc66293888ee6daa9
Keiichiro-Murayama <keiichiro.murayama@wemex.com>
2026-05-01 14:36:11 +0900

    finished Ex_03_13

---
+++ Ex_03_13/Circle.cs
@@ -0,0 +1,23 @@
+using System.Net.Http.Headers;
+
+namespace Ex_03_13;
+
+public class Circle
+{
+    public static double PI = 3.14;
+    int radius;
+
+    public Circle(int pradius)
+    {
+        this.radius = pradius;
+    }
+    public double AreaCalculation()
+    {
+        return radius * radius * PI;
+    }
+
+    public static double AreaCalculation(int radius)
+    {
+        return radius * radius* PI;
+    }
+}
---
+++ Ex_03_13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Ex_03_13;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var circle = new Circle(10);
+        System.Console.WriteLine($"5*5*3.14 = {circle.AreaCalculation()}");
+        System.Console.WriteLine($"5*5*3.14 ={Circle.AreaCalculation(5)}");
+        Circle.PI = 3;
+        System.Console.WriteLine($"5*5*3 ={circle.AreaCalculation()}");
+        System.Console.WriteLine($"5*5*3 ={Circle.AreaCalculation(5)}");
+      
+    }
+}
