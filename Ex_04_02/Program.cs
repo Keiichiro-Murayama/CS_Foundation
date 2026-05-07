9c6b3761ef3f217ec3c72f2851ab98fc5be6b663
Keiichiro-Murayama <keiichiro.murayama@wemex.com>
2026-05-07 11:10:52 +0900

    finished Ex_04_02

---
+++ Ex_04_02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex_04_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("年を入力してください->");
+        int year = int.Parse(Console.ReadLine());
+        if (DateTime.IsLeapYear(year))
+        {
+            System.Console.WriteLine($"{year}は閏年です。");
+        }else
+        {
+            System.Console.WriteLine($"{year}は閏年ではありません。");
+        }
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -1,37 +1,23 @@
-﻿using System.Runtime.CompilerServices;
+﻿string s1 = new string("B");
+string s2 = "ABC";
+string s3 = "DEFG";
+double d = 0.5;
 
-namespace Example;
-
-/// <summary>
-/// 
-/// </summary>
-static class Program
+Console.WriteLine($"s1:{s1}");
+Console.WriteLine($"s2:{s2}");
+Console.WriteLine($"s3:{s3}");
+Console.WriteLine($"d:{d}");
+Console.WriteLine();
+Console.WriteLine($"s1.Length:{s1.Length}");
+Console.WriteLine($"s2.Length:{s2.Length}");
+Console.WriteLine();
+Console.WriteLine($"s1.CompareTo(s2):{s1.CompareTo(s2)}");
+Console.WriteLine($"s2.CompareTo(s3):{s2.CompareTo(s3)}");
+Console.WriteLine();
+string[] ss = s2.Split(s1);
+foreach(string s in ss)
 {
-    static void Main(string[] args)
-    {
-        string s1 = new string("B");
-        string s2 = "ABC";
-        string s3 = "DEFG";
-        double d = 0.5;
-
-        Console.WriteLine($"s1:{s1}");
-        Console.WriteLine($"s2:{s2}");
-        Console.WriteLine($"s3:{s3}");
-        Console.WriteLine($"d:{d}");
-        Console.WriteLine();
-        Console.WriteLine($"s1.Length:{s1.Length}");
-        Console.WriteLine($"s2.Length:{s2.Length}");
-        Console.WriteLine();
-        Console.WriteLine($"s1.CompareTo(s2):{s1.CompareTo(s2)}");
-        Console.WriteLine($"s2.CompareTo(s3):{s2.CompareTo(s3)}");
-        Console.WriteLine();
-        string[] ss = s2.Split(s1);
-        foreach(string s in ss)
-        {
-            Console.WriteLine($"s2.Split(s1){s}");
-        }
-        Console.WriteLine();
-        Console.WriteLine($"string.Format(\"{{0:P}}\", d)}}:{string.Format("{0:P}", d)}");
-    }
+    Console.WriteLine($"s2.Split(s1){s}");
 }
-
+Console.WriteLine();
+Console.WriteLine($"string.Format(\"{{0:P}}\", d)}}:{string.Format("{0:P}", d)}");
